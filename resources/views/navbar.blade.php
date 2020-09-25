c69d5ae766414e0f179ec7a9a6ae072da0712b70
shimada <ue113241@gmail.com>
2020-09-25 03:26:41 +0000

    4 navbar レイアウト変更

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -2,14 +2,8 @@
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
             <a class="navbar__brand navbar__mainLogo" href="/"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-md-auto align-items-center">
-                    <li>
-                        <a class="nav-link commonNavIcon profile-icon" href="#"></a>
-                    </li>
                     <li>
                         <a class="btn btn-primary" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">logout</a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
