139e5255d96b19c1a6b036a3d12610d6ff55a018
shimada <ue113241@gmail.com>
2020-10-10 07:12:31 +0000

    13 postページ レイアウト

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -4,52 +4,53 @@
 <div class="panel-body">
 <!-- バリデーションエラーの場合に表示 --> 
 @include('common.errors')
-
-<div class="d-flex flex-column align-items-center mt-3">
-    <div class="col-xl-7 col-lg-8 col-md-10 col-sm-11 post-card">
-        <div class="card">
-            <div class="card-header">
-                新規投稿
-            </div>
-            <div class="card-body">
-                <form class="upload-images p-0 border-0" id="new_post" enctype="multipart/form-data" action="{{ url('posts')}}" accept-charset="UTF-8" method="POST">
-                {{csrf_field()}} 
-                    <div class="mb-3">
-                        <input id="post_image" type="file" name="post_image_path" accept="image/jpeg,image/gif,image/png" />
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="date">日付</label>
-                        <input class="form-control" id="date_picker" type="text" name="date" />
-                        <!-- jQueryの読み込み -->
-                        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-                        <!-- flatpickr読み込み -->
-                        <script src="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.4.3/flatpickr.min.js"></script>
-                        <script>
-                            (function($, window) {
-                                $(function() {
-                                    $('#date_picker').flatpickr();
-                                });
-                            })(jQuery, window);
-                        </script>
-                    </div>
-                    <!-- 都道府県のプルダウン入れたい -->
-                    <div class="form-group">
-                        <label for="date">都道府県</label>
-                        <select class="form-control" name="prefecture_id">
-                            @foreach ($prefectures as $prefecture)
-                                <option value="{{ $prefecture->id }}">{{ $prefecture->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="introduction">キャプション</label>
-                        <textarea class="form-control" name="caption"></textarea>
-                    </div>
-                    
-                    <input type="submit" name="commit" value="post" class="btn post-btn" data-disable-with="投稿する" />
-                </form>
+<div class="post-view-wrap">
+    <div class="d-flex flex-column align-items-center mt-3">
+        <div class="col-xl-7 col-lg-8 col-md-10 col-sm-11 post-card">
+            <div class="card">
+                <div class="card-header">
+                    新規投稿
+                </div>
+                <div class="card-body">
+                    <form class="upload-images p-0 border-0" id="new_post" enctype="multipart/form-data" action="{{ url('posts')}}" accept-charset="UTF-8" method="POST">
+                    {{csrf_field()}} 
+                        <div class="mb-3">
+                            <input id="post_image" type="file" name="post_image_path" accept="image/jpeg,image/gif,image/png" />
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="date">日付</label>
+                            <input class="form-control" id="date_picker" type="text" name="date" />
+                            <!-- jQueryの読み込み -->
+                            <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+                            <!-- flatpickr読み込み -->
+                            <script src="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.4.3/flatpickr.min.js"></script>
+                            <script>
+                                (function($, window) {
+                                    $(function() {
+                                        $('#date_picker').flatpickr();
+                                    });
+                                })(jQuery, window);
+                            </script>
+                        </div>
+                        <!-- 都道府県のプルダウン入れたい -->
+                        <div class="form-group">
+                            <label for="date">都道府県</label>
+                            <select class="form-control" name="prefecture_id">
+                                @foreach ($prefectures as $prefecture)
+                                    <option value="{{ $prefecture->id }}">{{ $prefecture->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="introduction">キャプション</label>
+                            <textarea class="form-control" name="caption"></textarea>
+                        </div>
+                        
+                        <input type="submit" name="commit" value="post" class="btn post-btn" data-disable-with="投稿する" />
+                    </form>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/post/view.blade.php
+++ resources/views/post/view.blade.php
@@ -3,26 +3,28 @@
 @include('footer')
 
 @section('content')
-<div class="col-md-8 col-md-2 mx-auto">
-    <div class="card-wrap">
-        <div class="card">
-            <div class="card-header align-items-center d-flex">
-                <a class="no-text-decoration" href="/mypage/{{ $post->user->id }}">
-                    @if ($post->user->profile_image_path)
-                        <img class="post-profile-icon round-img" src="{{ asset('storage/user_images/' . $post->user->profile_image_path) }}"/>
-                    @else
-                        <img class="post-profile-icon round-img" src="{{ asset('/images/blank_profile.png') }}"/>
-                    @endif
-                </a>
-                <a class="name-color no-text-decoration" title="{{ $post->user->name }}" href="/mypage/{{ $post->user->id }}">
-                    <strong>{{ $post->user->name }}</strong>
-                </a>
-            </div>
-
-            <img src="{{ asset('storage/post_images/' . $post->post_image_path) }}" class="card-img-top">
-            <div class="posts_text">
-                <p>{{ $post->date }}</p>
-                <p>{{ $post->caption }}</p>
+<div class="post-view-wrap">
+    <div class="col-md-8 col-md-2 mx-auto">
+        <div class="card-wrap">
+            <div class="card">
+                <div class="card-header align-items-center d-flex">
+                    <a class="no-text-decoration" href="/mypage/{{ $post->user->id }}">
+                        @if ($post->user->profile_image_path)
+                            <img class="post-profile-icon round-img" src="{{ asset('storage/user_images/' . $post->user->profile_image_path) }}"/>
+                        @else
+                            <img class="post-profile-icon round-img" src="{{ asset('/images/blank_profile.png') }}"/>
+                        @endif
+                    </a>
+                    <a class="name-color no-text-decoration" title="{{ $post->user->name }}" href="/mypage/{{ $post->user->id }}">
+                        <strong>{{ $post->user->name }}</strong>
+                    </a>
+                </div>
+    
+                <img src="{{ asset('storage/post_images/' . $post->post_image_path) }}" class="card-img-top">
+                <div class="posts_text">
+                    <p>{{ $post->date }}</p>
+                    <p>{{ $post->caption }}</p>
+                </div>
             </div>
         </div>
     </div>
