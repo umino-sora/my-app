1ae6d212ddd947574f8bdc95fe158a6b1652b656
shimada <ue113241@gmail.com>
2020-10-26 06:34:17 +0000

    21 heroku画像設定変更

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -58,10 +58,10 @@ class UsersController extends Controller
         $user = User::find($request->id);
         $user->name = $request->user_name;
         if ($request->profile_image_path !=null) {
-            // $request->profile_image_path->storeAs('public/user_images', $user->id . '.jpg');
-            // $user->profile_image_path = $user->id . '.jpg';
-            $path = Storage::disk('s3')->putFile('/',$form['profile_image_path'], 'public');
-            $user->profile_image_path = Storage::disk('s3')->url($path);
+            $request->profile_image_path->storeAs('public/user_images', $user->id . '.jpg');
+            $user->profile_image_path = $user->id . '.jpg';
+            // $path = Storage::disk('s3')->putFile('/',$form['profile_image_path'], 'public');
+            // $user->profile_image_path = Storage::disk('s3')->url($path);
         }
         $user->introduction = $request->introduction;
         $user->password = bcrypt($request->user_password);
